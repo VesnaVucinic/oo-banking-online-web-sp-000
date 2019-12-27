c07f355398706b46c6435822eaa52ab9b477647a
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-12-27 19:57:47 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+#can initialize a Bank Account
+   # initializes with a name
+    #always initializes with balance of 1000
+    #always initializes with a status of 'open'
+    #can't change its name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+# can deposit money into its account
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+ # can display its balance
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+# can close its account
+  def close_account
+    self.status = "closed"
+  end
+  
+# is valid with an open status and a balance greater than 0
+  def valid?
+    balance > 0 && status == "open"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
+
 class Transfer
-  # your code here
+  attr_reader :transfer, :sender, :receiver, :status
+  
+  def initialize(transfer, sender, receiver, status = "pending")
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = status
+  end
+    
+
 end
